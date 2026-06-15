f111af5d7ef5d3748c7ccf9adb72d9e95f49a642
Avis <tsangching824@gmail.com>
2026-06-15 16:00:32 +0800

    update the failed playing music problem

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let input_buffer = "";
 const gaming = document.getElementById("gaming");
 
 document.addEventListener("DOMContentLoaded",()=>{
+    music.muted = false; 
     gaming.loop = true;
     gaming.volume = 0.4;
     gaming.play();
